5e331e79bc9ed8764b123c25138a52fe3d9a6b1f
servernoj <simon.baev@globallogic.com>
2021-08-13 15:44:20 -0700

    1 file modified

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello World Vite!</h1>
+  <h1>Hello World Again Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
